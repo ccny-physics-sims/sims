1684c2bda2e1fe2cebc56ad6136d0aeffc6964cc
hedbergj <james@smile2.local>
2017-10-10 10:00:50 -0400

    two-body orbiter

---
+++ two-body-orbits/sketch.js
@@ -0,0 +1,86 @@
+var orbiters=[];
+var gravity =5;
+var totalMass;
+var Trails = [];
+
+var SolarSystem;
+function setup(){
+  frameRate(30);
+  createCanvas(windowWidth, windowHeight);
+  v1 = 1;
+  massRatio = 2;
+
+
+ SolarSystem = new GravSystem(orbiters);
+
+mass1 =  orbiters.push(new Orbiter(createVector(width/2,-200+height/2),createVector(v1,0),createVector(0,0),100, random(50,200)));
+mass2 = orbiters.push(new Orbiter(createVector(width/2,height/2),createVector(-v1/massRatio,0),createVector(0,0),100*massRatio, random(50,200)));
+
+ }
+
+
+
+function draw(){
+background(255);
+
+  for (var k = 0; k < 4; k++) { // increase the greater than value to increase simulation step rate
+      SolarSystem.do_physics(1.0 / 8); // increase the divisor to increase accuracy and decrease simulation speed
+  }
+
+  for (i=0;i<orbiters.length;i++){
+    orbiters[i].display();
+  }
+
+  COM();
+
+  for (var i = Trails.length-1; i >= 0; i--) {
+    var p = Trails[i];
+    p.run();
+    if (p.isDead()) {
+      //remove the TrailDot
+      Trails.splice(i, 1);
+    }
+  }
+
+}
+
+
+function COM(){
+  //displays the center of mass of the system
+  m1 = createVector(0,0)
+
+  totalMass=0;
+
+
+  for(i=0;i<orbiters.length;i++){
+    totalMass=totalMass+orbiters[i].mass;
+    m1 = p5.Vector.add(m1,p5.Vector.mult(orbiters[i].position,orbiters[i].mass))
+  }
+
+  com = p5.Vector.div(m1,totalMass);
+  push();
+  fill(0)
+  stroke(0)
+  line(com.x-5,com.y,com.x+5,com.y,)
+  line(com.x,com.y-5,com.x,com.y+5,)
+  pop();
+}
+
+
+
+
+function windowResized() {
+    // Resize necessary elements to fit new window size
+    resizeCanvas(windowWidth, windowHeight); // width and height system variables updated here
+  }
+
+function keyTyped(){
+ if (key === 'c'){
+    for ( i = orbiters.length-1; i >= 0; i--){
+      orbiters.splice(i,1);
+    }
+    // for ( i = Trails.length-1; i >= 0; i--){
+    //   Trails.splice(i,1);
+    // }
+  }
+}
