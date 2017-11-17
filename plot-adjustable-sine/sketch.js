be8f8be1bbfd54bd8918403224b6f14fa6d5b796
hedbergj <james@smile2.local>
2017-11-17 09:00:22 -0500

    add sine function to math

---
+++ plot-adjustable-sine/sketch.js
@@ -0,0 +1,99 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(250);
+  frameRate(30);
+  textSize(18)
+  //lets make an array to fill
+  y = new Array(windowWidth);
+
+  amplitudeControl = createSlider(-200,200,50,0);
+  amplitudeControl.position(20,50)
+  amplitudeControl.parent('sketch-holder')
+  amplitudeControl.class("sim-slider");
+  amplitudeControlLabel = createP("Amplitude");
+  amplitudeControlLabel.position(20,20);
+  amplitudeControlLabel.parent('sketch-holder')
+
+
+  omegaControl = createSlider(1,4,1,.01);
+  omegaControl.position(20,120)
+  omegaControl.parent('sketch-holder')
+  omegaControl.class("sim-slider");
+  omegaControlLabel = createP("Omega");
+  omegaControlLabel.position(20,90);
+  omegaControlLabel.parent('sketch-holder')
+}
+
+
+function draw() {
+  background(250)
+  stroke(0)
+  //move things to the middle
+  translate(80, height / 2)
+  //x axis
+  line(0, 0, width, 0)
+  line(0,-200,0,200)
+
+  amplitude = amplitudeControl.value();
+  omega = omegaControl.value();
+  //calculate this points
+  calcFunction();
+  //display discrete points
+  renderPoints();
+  //display connected line
+  renderLine();
+
+  showMaxAmplitude();
+  showPeriod();
+}
+
+function calcFunction() {
+  //this function fills the aray with values
+  for (var x = 0; x < y.length; x += 1) {
+    y[x] = amplitude * Math.sin(.01 * x*omega)
+  }
+
+}
+
+function renderPoints() {
+  //this function puts ellipses at all the positions defined above.
+  noStroke()
+      fill(0);
+  for (var x = 0; x < y.length; x += 10) {
+
+    ellipse(x, -y[x], 5, 5);
+  }
+}
+
+function renderLine() {
+  //this function puts a line through all the positions defined above.
+
+  push();
+  noFill();
+  stroke('red');
+
+  beginShape();
+  for (var x = 0; x < y.length; x += 2) {
+    curveVertex(x, -y[x]);
+  }
+  endShape();
+  pop();
+}
+function showMaxAmplitude(){
+  stroke(0)
+  line(00,-amplitude,10,-amplitude)
+  line(0,amplitude,10,amplitude)
+  noStroke()
+  text('A (max)',-70,-abs(amplitude)+5)
+  text('A (min)',-70,abs(amplitude)+5)
+}
+
+function showPeriod(){
+  stroke(0)
+  translate(0,-80)
+  line(PI/(2*.01*omega),0,PI/(2*.01*omega),-30)
+  line(5*PI/(2*.01*omega),0,5*PI/(2*.01*omega),-30)
+  line(PI/(2*.01*omega),-15,5*PI/(2*.01*omega),-15)
+  noStroke()
+  text('T (period)',(PI/(2*.01*omega)+5*PI/(2*.01*omega))/2,-30)
+}
