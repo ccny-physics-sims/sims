728809feb6f0a4eca295adf3e52435d82dda6ec0
cdot18 <cdoty25@gmail.com>
2018-01-25 16:26:02 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,396 +1,309 @@
-//Global variables
-var pend_length=150
-var g = .2
-var omega
-var x_axis_1, x_axis_2, x_axis_3
+var xspacing = 1 //Why?
+var Re //Store values of REAL part of wavefunction
+var Im //Store values of IMAGINARY part of wavefunction
+var p //Store values of Mag(Psi)^2
+var theta // theta = wt (intialized to zero when init() function is called)
+var wavelength1 //Wavelength depends on E-V
+var wavelength2 //Wavelength2 should be longer in region2 if E-V is smaller 
+var dx1 //The amount by which the argument of cos(kx+-wt) increases (for a fixed time) from one point to the next in region 1
+var dx2 //Region 2
+var omega  //Needs to be changed when E changes
+var A_I = 1 //Amplitude of incident wave
+var A_R
+var A_T
+var E //Total energy of incident particle 
+var V0 //Height of potential barrier
+var E_slider //Controls value of E
+var V_slider
+var ground // y coordinate of line marking V = 0
+var x_axis //y coordinate of x-axis for plot
+var img1, img2
 
-//boolean variables
-var running
-var paused
-var n1, n2, n3
 
-//buttons and slider variable
-var start_btn
-var pause_btn
-var reset_btn
-var resume_btn
-var n1_btn;//To select IC's which will excite the first normal mode
-var n2_btn;//To select IC's which will excite second normal mode
-var n3_btn;
-/*var slider1;
-var slider2;
-var slider3;*/
 
 function setup() {
   
-  x_axis_1 = 90
-  x_axis_2 = 270
-  x_axis_3 = 450
   
-  n1 = false
-  n2 = false
-  n3 = false
+  frameRate(45)//60 too fast??
   
-  createCanvas(windowWidth,windowHeight);
+  img1 = loadImage("Left_Equation.jpg")
+  img2 = loadImage("Right_Equation.jpg")
+
   
-  //Sliders to control initial conditions
-  /*slider1=createSlider(-22.5,22.5,0,.1);//
-  slider1.position(175,60);
-  slider2=createSlider(-22.5,22.5,0,.1);
-  slider2.position(175,210);
-  slider3=createSlider(-22.5,22.5,0,.1);
-  slider3.position(175,360);*/
+  //V0 = 150//height of potential bump in pixels
   
-  //First normal mode button
-  n1_btn=createButton("First Normal Mode");
-  n1_btn.position(50,40);
-  n1_btn.mouseClicked(N1);
+  E_slider = createSlider(0+4,280,150+126/2 ,1)
+  V_slider = createSlider(0,300,150,1)
+  //E_slider = createSlider(1,100,100,1)
+  //V_slider = createSlider(0,100,0,1)
+  Re_checkbox = createCheckbox('Re[Psi]', true)
+  Im_checkbox = createCheckbox('Im[Psi]', false)
+  P_checkbox = createCheckbox('Probability ( |Psi|^2 )', false)
+  Re_checkbox.changed(Re_box_changed)
+  Im_checkbox.changed(Im_box_changed)
+  P_checkbox.changed(P_box_changed)
+  /*Left_checkbox = createCheckbox('Left',true)
+  Right_checkbox = createCheckbox('Right',false)
+  Left_checkbox.changed(Left_box_changed)
+  Right_checkbox.changed(Right_box_changed)*/
   
-  n2_btn=createButton("Second Normal Mode");
-  n2_btn.position(50,70);
-  n2_btn.mouseClicked(N2);
+  /*arrow_A = new Arrow(createVector(0,0),createVector(1,1))
+  arrow_A.color = 'black'
+  arrow_A.width = 10
+  arrow_B = new Arrow(createVector(0,0),createVector(1,1))
+  arrow_B.color = 'black'
+  arrow_B.width = 10
+  arrow_C = new Arrow(createVector(0,0),createVector(1,1))
+  arrow_C.color = 'black'
+  arrow_C.width = 10*/
   
-  n3_btn=createButton("Third Normal Mode");
-  n3_btn.position(50,100);
-  n3_btn.mouseClicked(N3);
+  init()
   
-  //Start button
-  start_btn= createButton("Start");
-  start_btn.position(50,180);
-  start_btn.mouseClicked(start);//Calls start function on click event
+  reset()
   
-  //Pause button
-  pause_btn=createButton("Pause");
-  pause_btn.position(50,210);
-  pause_btn.mouseClicked(pause);//Calls pause function on click event
-  
-  //Resume button
-  resume_btn=createButton("Resume");
-  resume_btn.position(50,240);
-  resume_btn.mouseClicked(resume);//
-  
-  //Reset button
-  reset_btn=createButton("Reset");
-  reset_btn.position(50,270);
-  reset_btn.mouseClicked(reset);//Calls reset function on click event
-  
-  reset();
 }
 
 function draw() {
   
-background(255);
-
-makePretty() //Labels and stuff
-
-framePlots()
-
-if(n1||n2||n3){
-  renderWaves(omega,1,600)
-}
-
-  //Start not pressed
-  if(running==false){
-     /*p1.angle1=radians(slider1.value());
-     p1.angle2=radians(slider2.value());
-     p1.angle3=radians(slider3.value());*/
-     
-     if(n1){
-       p1.angle3=radians(22.5)
-       p1.angle2=radians(22.5*.792113)
-       p1.angle1=radians(22.5*.61307)
-     }
-     else if(n2){
-       p1.angle3=radians(22.5)
-       p1.angle2=radians(-22.5*.14714)
-       p1.angle1=radians(-22.5*.416993)
-     }
-     else if(n3){
-       p1.angle3=radians(-22.5*.466206)
-       p1.angle2=radians(22.5)
-       p1.angle1=radians(-22.5*.607913)
-     }
-     p1.display();
+  background(255)
+  
+  
+    
+  if (E_slider.value()==E && V_slider.value()==V0){ //Sliders haven't changed
+    calcWave()
+    renderWave()
+    DrawStuff()//Title,labels,frames,axis,etc.
   }
   
-  //Start was pressed
-  else {
+  else { //Slider changed so init() function is called
+    E = E_slider.value()
+    V0 = V_slider.value()
+    reset()
+    calcWave()
+    renderWave()
+    DrawStuff()
     
-    if(paused==false)
-    {
-       p1.go();
-    }
-       
-    else if(paused==true)
-    {
-       p1.display();
-    }
   }
   
-} 
- 
-//Triple pendulum with equal lengths and equal masses
-function Triple_Pendulum(origin_, r_,theta10_,theta20_,theta30_) {
-  // Fill all variables
-  this.origin = origin_.copy();
-  this.position1 = createVector();
-  this.position2= createVector();
-  this.position3= createVector();
-  this.r = r_;
-  this.angle1 = theta10_;
-  this.angle2= theta20_;
-  this.angle3= theta30_;
-  this.aVelocity1 = 0.0;
-  this.aVelocity2 = 0.0;
-  this.aVelocity3 = 0.0;
-  this.aAcceleration1 = 0.0;
-  this.aAcceleration2 = 0.0;
-  this.aAcceleration3 = 0.0;
-  this.ballr = 25.0;      // Arbitrary ball radius
+  
+}
 
-  this.go = function() {
-    this.update();
-    this.display();
+function calcWave(){
+  wavelength1 = 400/sqrt(E)
+  
+  if(E>V0){
+    wavelength2 = 400/sqrt(E-V0)
   }
-
-  // Function to update position
-  this.update = function() {
-    //var g= .2;    // Arbitrary constant  
-    
-    //Coupled oscillator equations of motion for small angles
-    this.aAcceleration1 = -3*(g/this.r)*this.angle1 + 2*(g/this.r)*this.angle2;
-    this.aAcceleration2 = 3*(g/this.r)*this.angle1 - 4*(g/this.r)*this.angle2+(g/this.r)*this.angle3;//Small angle approximation
-    this.aAcceleration3 = 2*(g/this.r)*this.angle2 -2*(g/this.r)*this.angle3;
-    
-    //Increment velocity and position
-    this.aVelocity1 += (this.aAcceleration1);  
-    this.aVelocity2 += (this.aAcceleration2);
-    this.aVelocity3 += (this.aAcceleration3);
-    this.angle1 += (this.aVelocity1); 
-    this.angle2 += (this.aVelocity2);
-    this.angle3 += (this.aVelocity3);
+  else{
+    wavelength2 = 400/sqrt(V0-E)
   }
-
-  this.display = function() {
-    this.position1.set(this.r*sin(this.angle1), this.r*cos(this.angle1), 0);         // Polar to cartesian conversion
-    this.position1.add(this.origin);                                               // Make sure the position is relative to the pendulum's origin
-    this.position2.set(this.r*sin(this.angle2), this.r*cos(this.angle2), 0);
-    this.position2.add(this.position1); //position of mass 2 is relative position of mass 1
-    this.position3.set(this.r*sin(this.angle3), this.r*cos(this.angle3), 0);
-    this.position3.add(this.position2);
+  
+  dx1 = ( (TWO_PI) / wavelength1 ) * xspacing
+  dx2 = ( (TWO_PI) / wavelength2 ) * xspacing
+  A_R = ( (wavelength2 - wavelength1) / (wavelength1 + wavelength2) ) * A_I
+  A_T = ( 2*(wavelength2) / (wavelength1 + wavelength2) ) * A_I 
+  theta += .01 * omega
+  var x0 = -floor(N/2) 
+  for ( var i=0; i<N ; i++ ){
+    if (i<=floor(N/2)){ 
+      
+        Re[i] = A_I*cos(-theta+(i+x0)*dx1)+A_R*cos(-theta-(i+x0)*dx1)
+        Im[i] = A_I*sin(-theta+i*dx1)+A_R*sin(-theta-i*dx1)
+        p[i] = (sq(A_I)+sq(A_R))+(2*A_I*A_R)*(sq(cos(i*dx1))-sq(sin(i*dx1)))
+      
+      
+    }
     
-    stroke(0);
-    strokeWeight(1);
-    
-    // Draw the arms
-    line(this.origin.x, this.origin.y, this.position1.x, this.position1.y);
-    line(this.position1.x, this.position1.y, this.position2.x, this.position2.y);
-    line(this.position2.x, this.position2.y, this.position3.x, this.position3.y);
-    ellipseMode(CENTER);
-    fill(127);
-    
-    // Draw the balls
-    ellipse(this.position1.x, this.position1.y, this.ballr, this.ballr);
-    ellipse(this.position2.x, this.position2.y, this.ballr, this.ballr);
-    ellipse(this.position3.x, this.position3.y, this.ballr, this.ballr);
+    if (i>floor(N/2)){
+      
+      if (E<V0){
+        Re[i] = A_T*Math.exp(-(i+x0)*dx2)
+        Im[i] = 0
+        p[i] = sq(A_T*Math.exp(-(i+x0)*dx2))
+      }
+      
+      else{
+        Re[i] = A_T*cos(-theta+(i+x0)*dx2)
+        Im[i] = A_T*sin(-theta+(i+x0)*dx2)
+        p[i] = sq(A_T)
+        
+      }
+    }
     
   }
 }
 
-function start(){
-  running=true;
-  return
-}
 
-function pause(){
-  if(running==true){
-    paused=true;
-  }
-  return
-  //if sim not running pressing pause shouldnt do anything
-}
+function renderWave() {
 
-function resume(){
-  if(paused==true){
-    paused=false;
-  }
-  //if sim isn't paused pressing resume doesn't do anything
-  return
+push()
+  noFill()
+  strokeWeight(2)
+  
+  beginShape()
+    if (Re_checkbox.checked()){
+      stroke('red')
+      for (var x = 0; x < N; x+=1) {
+      curveVertex(x*xspacing, x_axis-40*Re[x])
+      }
+    }
+    else if (Im_checkbox.checked()){
+      stroke('blue')
+      for (var x = 0; x < N; x+=1) {
+      curveVertex(x*xspacing, x_axis-40*Im[x])
+      }
+    }
+    else{
+      for (var x = 0; x < N; x+=1) {
+      curveVertex(x*xspacing, x_axis-40*p[x])
+      }
+    }
+  endShape()
+pop()
 }
 
 function reset(){
-  n1 = n2 = n3 = false
-  running = paused = false;
-  p1 = new Triple_Pendulum(createVector(425,10),pend_length,0,0,0);
-  /*slider1.value(0);
-  slider2.value(0);
-  slider3.value(0);*/
-  i = 0;
-  
-  return
+  omega=.1*E 
+  theta = 0
 }
 
-//Normal mode 1 (equal lengths and masses)
-function N1(){
-  if(running==false){
-    n1 = true
-    n2 = false
-    n3 = false
-    omega = sqrt(g/pend_length)
-  }
+function DrawStuff() {
+  //Make top half grey
+  push();
+   noStroke();
+   fill(240)
+   rect(0,0,width,ground)
+  pop()
+  
+  push()
+    textSize(20)
+    text("1-D Quantum Scattering",width/2-100,20)
+  pop()
+  
+  image(img1,150,height/2+10)
+  image(img2,800,height/2+10)
     
-  else{
-    return
-  }
   
-  /*slider3.value(22.5);
-  slider2.value(22.5*.792113);
-  slider1.value(22.5*.613037);*/
-}
-
-
-
-function N2(){
-  if(running==false){
-    n1 = false
-    n2 = true
-    n3 = false
-    omega = sqrt(g/pend_length)*1.5
-  }
-    
-  else{
-    return
-  }
-  /*slider3.value(22.5);
-  slider2.value(-22.5*.14714);
-  slider1.value(-22.5*.416993);*/
-}
-
-function N3(){
- if(running==false){
-    n1 = false
-    n2 = false
-    n3 = true
-    omega = sqrt(g/pend_length)*2.2
-  }
-    
-  else{
-    return
-  }
-  /*slider3.value(-22.5*.466206);
-  slider2.value(22.5);
-  slider1.value(-22.5*.607913);*/
-}
-
-function renderWaves(omega_,x_spacing_,wave_width_){
-    var N = floor(wave_width_/x_spacing_)
-    var y = new Array(N)
-    var d_theta = omega_*x_spacing_
-    var A1,A2,A3
-    if (n1){
-      A1=22.5*.613037
-      A2=22.5*.792113
-      A3=22.5
-    }
-    else if(n2){
-      A1=-22.5*.416993
-      A2=-22.5*.14714
-      A3=22.5
-    }
-    else{
-      /*slider3.value(-22.5*.466206);
-  slider2.value(22.5);
-  slider1.value(-22.5*.607913);*/
-      A1=-22.5*.607913
-      A2=22.5
-      A3=-22.5*.466206
-    }
-    //=============================================================
-    beginShape()
-    push()
-    for( var i=0; i<=N; i++ ) {
-      noFill()
-      curveVertex(i*x_spacing_+600,x_axis_1-2*(A1)*cos(i*d_theta))
-      pop()
-    }
-    endShape()
   
-    beginShape()
-    push()
-    for( var i=0; i<=N; i++ ) {
-      noFill()
-      curveVertex(i*x_spacing_+600,x_axis_2-2*(A2)*cos(i*d_theta))
-      pop()
-    }
-    endShape()
+  //Axis to graph wavefunction and probability density
+  push()
+   stroke(10)
+   strokeWeight(1)
+   line(0,x_axis,width,x_axis)//x-axis
+   line(width/2,ground+10,width/2,height-10)//y-axis
+  pop()
   
-    beginShape()
-    push()
-    for( var i=0; i<=N; i++ ) {
-      noFill()
-      curveVertex(i*x_spacing_+600,x_axis_3-2*(A3)*cos(i*d_theta))
-      pop()
-    }
-    endShape()
+  //Arrows and labels to show incident, reflected, and transmitted wavefunctions
+  /*arrow_A.update()
+  arrow_A.display()
+  arrow_B.update()
+  arrow_B.display()
+  arrow_C.update()
+  arrow_C.display()*/
+
+  
+  //Draw potential and horizontal line to indicate energy
+  push();
+   stroke(51);
+   strokeWeight(4);
+   line(0,ground, width/2, ground) //Line to mark potential for x<0 V=0
+   line(width/2,ground,width/2,ground-V0) //Vertical line to show height change of potential barrier at x=0
+   line(width/2,ground-V0,width,ground-V0) //Horizontal line showing height of pontetal after x=0
+   stroke('green')
+   line(0,ground-E,width,ground-E)
+  pop();
+  
+  //Label V0
+  push()
+   fill('black')
+   textSize(16)
+   text('V0',width/2-25,ground-(V0+3))
+  pop()
+  
+  //Label Energy line
+  push()
+   fill('green')
+   textSize(16)
+   text('E',10,ground-E-5)
+  pop()
 }
 
-function framePlots(){
-  
-push()
-  noStroke()
-  fill(250)
-  rect(600,15,600,150)
-  rect(600,195,600,150)
-  rect(600,375,600,150)
-pop()
-
-push()
-  fill('black')
-  textSize(14)
-  text("Time",1200,x_axis_1+5)
-  text("Time",1200,x_axis_2+5)
-  text("Time",1200,x_axis_3+5)
-  text("Angle 1",585,10)
-  text("Angle 2",585,190)
-  text("Angle 3",585,370)
-pop()
-
-push()
-  line(600,x_axis_1,1200,x_axis_1)
-  line(600,15,600,165)
-  line(600,x_axis_2,1200,x_axis_2)
-  line(600,195,600,345)
-  line(600,x_axis_3,1200,x_axis_3)
-  line(600,375,600,525)
-pop()
+function Right_box_changed(){
+  if(Left_checkbox.checked()){
+    Left_checkbox.checked(false)
+  }
 }
 
-function makePretty(){
-
-push()
-  fill('black')
-  rect(395,10,60,5)
-pop()
-  
-push()
-  fill(240)
-  rect(40,10,215,125)
-pop()
-
-push()
-  fill('black')
-  textSize(18)
-  text("Select Initial Conditions:",50,30)
-pop()
-
-push()
-  fill(240)
-  rect(40,150,215,150)
-pop()
-
-push()
-  fill('black')
-  textSize(18)
-  text("Run Simulation:",50,170)
-pop()
+function Left_box_changed(){
+  if(Right_checkbox.checked()){
+    Right_checkbox.checked(false)
+  }
 }
+
+function Im_box_changed(){
+  if (Im_checkbox.checked()){
+    Re_checkbox.checked(false)
+    P_checkbox.checked(false) 
+  }
+ 
+}
+
+function Re_box_changed(){
+  if (Re_checkbox.checked()){
+    Im_checkbox.checked(false)
+    P_checkbox.checked(false)
+  }
+}
+
+function P_box_changed(){
+  if (P_checkbox.checked()){
+    Re_checkbox.checked(false)
+    Im_checkbox.checked(false)
+  }
+}
+
+function init(){
+  
+  var cnv = createCanvas(windowWidth,windowHeight);//Address resizing issue later
+  
+  //Initialize variables
+  N = floor(width/xspacing) //Num points to calculate value of wavefunction
+  ground = height/2 //V=0
+  x_axis = .75*height
+  
+  //Create arrays to store values of wavefunction at N poi
+  Re = new Array(N) //Real part of Psi
+  Im = new Array(N) //Imaginary part of Psi
+  p = new Array(N) //Psi^2
+  
+  
+  E_slider.position(.3*width, .33*height)
+  E = E_slider.value()
+  V_slider.position(.6*width,.33*height)
+  V0 = V_slider.value()
+  
+  
+  Re_checkbox.position(.1*width,.9*height)
+  Im_checkbox.position(.20*width,.9*height)
+  P_checkbox.position(.3*width,.9*height)
+  
+  /*Left_checkbox.position(.7*width,.9*height)
+  Right_checkbox.position(.8*width,.9*height)*/
+  
+  /*arrow_A.origin = createVector(.3*width,.35*height)
+  arrow_A.target = createVector(.4*width,.35*height)
+  
+  arrow_B.origin = createVector(.4*width,.4*height)
+  arrow_B.target = createVector(.3*width,.4*height)
+  
+  arrow_C.origin = createVector(.6*width,.4*height)
+  arrow_C.target = createVector(.7*width,.4*height)*/
+  
+}
+
+function windowResized() {
+  init()
+  reset()
+}
+
+
