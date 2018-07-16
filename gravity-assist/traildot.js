195635c39dc2651b68e112d3fb71fc5d096277d9
hedbergj <jhedberg@ccny.cuny.edu>
2018-07-16 15:34:41 -0400

    gravity assist

---
+++ gravity-assist/traildot.js
@@ -0,0 +1,36 @@
+//TrailDots class
+
+var TrailDot = function(position, lifespan) {
+  this.position = position.copy();
+  this.lifespan = lifespan;
+  this.originalLife = lifespan
+};
+
+TrailDot.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+TrailDot.prototype.update = function(){
+  this.lifespan -= 1;
+};
+
+// Method to display
+TrailDot.prototype.display = function() {
+  push();
+
+  fill(0,0,0,255*this.lifespan/this.originalLife);
+  noStroke();
+  ellipse(this.position.x, this.position.y, 2,2);
+  pop();
+};
+
+// Is the TrailDot still useful?
+TrailDot.prototype.isDead = function(){
+  if (this.lifespan < 0.0) {
+      return true;
+  } else {
+    return false;
+  }
+};
