d1369924aa2df6478ff2eb06f1b1eda4709d8863
James Hedberg <jhedberg@ccny.cuny.edu>
2020-11-24 10:01:07 -0500

    pulley-box

--- pulley-box/sketch.js
+++ pulley-box/sketch.js
@@ -1,10 +1,10 @@
 
-var Trails = [];
+
 var running = false;
 
 
 function setup(){
-canvas = createCanvas(windowWidth*.9, windowHeight*.9);
+canvas = createCanvas(windowWidth*.8, windowHeight*.7);
 canvas.parent('sketch-holder');
 
   c = 0;
@@ -20,28 +20,29 @@ canvas.parent('sketch-holder');
   reseter.position(20,30);
   reseter.class("sim-button")
 
-  discMassSlider = createSlider(1, 500, 100, 1);
-  discMassSlider.position(20,110);
+  discMassSlider = createSlider(0, 50, 10, 1);
+  discMassSlider.position(width*.01,height*.2);
   discMassSlider.parent('sketch-holder');
-  discMassSlider.class("sim-slider red");
+  discMassSlider.class("sim-slider");
   discMassSliderLabel = createP()
-  discMassSliderLabel.position(20,80)
+  discMassSliderLabel.position(10,height*.23);
   discMassSliderLabel.parent('sketch-holder');
 
   boxMassSlider = createSlider(1, 10, 1, 1);
-  boxMassSlider.position(20,190);
+  boxMassSlider.position(width*.01,height*.35);
   boxMassSlider.parent('sketch-holder');
-  boxMassSlider.class("sim-slider red");
+  boxMassSlider.class("sim-slider");
   boxMassSliderLabel = createP()
-  boxMassSliderLabel.position(20,160)
+  boxMassSliderLabel.position(10,height*.38);
   boxMassSliderLabel.parent('sketch-holder');
 
   accelerationLabel = createP();
-  accelerationLabel.position(20,240);
+  accelerationLabel.position(10,height*.44);
   accelerationLabel.parent('sketch-holder');
+  accelerationLabel.style('font-size', '2em')
 
   //speedRoller = 10;
-  g = .5;
+  g = .1;
   startx = 2*width/3;
   starty = 200;
   massBox = boxMassSlider.value();
@@ -50,10 +51,10 @@ canvas.parent('sketch-holder');
   vel = createVector(0,0)
   accel = createVector(0,0)
 
-  box = new KineticMass(pos,vel,accel,1,'red');
-  box.outline = color('rgba(255, 0, 0, 1)');
-  box.limit = 2000;
-  box.tail = false;
+  fallingBox = new KineticMass(pos,vel,accel,1,'red');
+  fallingBox.outline = color('rgba(255, 0, 0, 1)');
+  fallingBox.limit = 2000;
+  fallingBox.tail = false;
 
 
   w = new wheel(startx-50,starty-50,100);
@@ -65,7 +66,11 @@ canvas.parent('sketch-holder');
   w.rimColor = color('rgba(0,0,0,1)');
   w.spokeColor = color('rgba(0,0,0,1)');
   w.wheelColor = color('rgba(0,0,0,.1)');
+  w.rimThickness = map(massDisc,1,50,1,5)*.5;
   rectMode(CENTER)
+  discMassSliderLabel.html("Mass of Pulley: "+massDisc.toFixed(0))
+  boxMassSliderLabel.html("Mass of Box: "+massBox.toFixed(0))
+  accelerationLabel.html("Acceleration: <span style='color: red'>"+(2*massBox/(2*massBox+massDisc)).toFixed(3)+" <i>g</i> </span>");
   //noLoop();
 }
 
@@ -78,37 +83,38 @@ function draw(){
   massDisc = discMassSlider.value();
   discMassSliderLabel.html("Mass of Pulley: "+massDisc.toFixed(0))
   boxMassSliderLabel.html("Mass of Box: "+massBox.toFixed(0))
-  accelerationLabel.html("Acceleration: "+(2*massBox/(2*massBox+massDisc)).toFixed(3)+"g");
+  accelerationLabel.html("Acceleration: <span style='color: red'>"+(2*massBox/(2*massBox+massDisc)).toFixed(3)+" <i>g</i> </span>");
 
 
   w.draw();
    //get speed from slider
-  w.ang_speed += box.acceleration.y/50
+  w.ang_speed += fallingBox.acceleration.y/50
+  w.rimThickness = map(massDisc,1,50,1,5)*.5;
   stroke(0)
   strokeWeight(2)
-  line(startx,starty-50,box.position.x,box.position.y)
+  line(startx,starty-50,fallingBox.position.x,fallingBox.position.y)
 
-  if (box.position.y>(height-16)){
-    box.velocity.y = 0;
-    box.acceleration.y = 0;
+  if (fallingBox.position.y >= (height-3-(10+massBox*2)/2)){
+    fallingBox.velocity.y = 0;
+    fallingBox.acceleration.y = 0;
     w.ang_speed = 0;
     //noLoop();
   }
-  box.update()
-  box.display()
+  fallingBox.update()
+  fallingBox.display()
   //fill('red')
-  rect(box.position.x, box.position.y, 20,20)
+  rect(fallingBox.position.x, fallingBox.position.y, 10+massBox*2,10+massBox*2)
 };
 
-
-function windowResized() {
-    // Resize necessary elements to fit new window size
-     resizeCanvas(windowWidth, windowHeight); // width and height system variables updated here
-  }
+//
+// function windowResized() {
+//     // Resize necessary elements to fit new window size
+//      resizeCanvas(windowWidth, windowHeight); // width and height system variables updated here
+//   }
 function resetTheBalls(){
-
-  box.position.y = starty;
-  box.acceleration.y = g*(2*massBox/(2*massBox+massDisc))
+  loop()
+  fallingBox.position.y = starty;
+  fallingBox.acceleration.y = g*(2*massBox/(2*massBox+massDisc))
   //noLoop();
   //running = false;
 }
