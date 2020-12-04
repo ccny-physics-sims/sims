8d8832b2356be39106b943e5d2359f7fc6c32632
James Hedberg <jhedberg@ccny.cuny.edu>
2020-12-04 07:17:21 -0500

    tru acc fix

--- force-motion/sketch.js
+++ force-motion/sketch.js
@@ -1,5 +1,55 @@
 var whitish = 200;
 let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+let hasSensorPermission = !(DeviceOrientationEvent.requestPermission || DeviceMotionEvent.requestPermission);
+
+function begPermission(){
+  if (DeviceOrientationEvent.requestPermission){
+    DeviceOrientationEvent.requestPermission()
+    .then(response => {
+      if (response == 'granted') {
+
+        if (DeviceMotionEvent.requestPermission){
+          DeviceMotionEvent.requestPermission()
+          .then(response => {
+            if (response == 'granted') {
+              hasSensorPermission = true;
+              window.ondevicemotion = function(event) {
+
+                // devices such as iOS 13 doesn't have accleration,
+                // instead they have accelerationIncludingGravity, producing inconsistent behavior.
+                // here we patch it by canelling out the gravitational acclerration.
+                // alternatively, we add the gravitational acceleration to android phones.
+                if (!event.acceleration){
+
+                  // compute gravitational acceleration's component on X Y Z axes based on gyroscope
+                  // g = ~ 9.80665
+                  let grav = TRFM(MULT(
+                    ROTY(radians(rotationY)),
+                    ROTX(radians(rotationX))
+                  ),[0,0,-9.80665]);
+
+                  accX =  (event.accelerationIncludingGravity.x+grav[0]);
+                  accY =  (event.accelerationIncludingGravity.y+grav[1]);
+                  accZ =  (event.accelerationIncludingGravity.z-grav[2]);
+
+                  // p5 appears to be doubling the acceleration:
+                  // https://github.com/processing/p5.js/blob/main/src/events/acceleration.js#L647
+                  accX *= 2;
+                  accY *= 2;
+                  accZ *= 2;
+                }
+
+              }
+            }
+          })
+          .catch(alert)
+        }
+      }
+    })
+    .catch(alert)
+  }
+}
+
 
 
 function setup() {
@@ -30,6 +80,15 @@ accelVector.showComponents = false;
 
 function draw() {
 //flicker
+if (!hasSensorPermission){
+   textSize(18);
+   background(0);
+   fill(255);
+   noStroke();
+   textAlign(CENTER);
+   text("Tap the screen\nto request sensor permissions...",width/2,height/2);
+   return;
+ }
 
 background(random(16,23));
 
@@ -49,14 +108,12 @@ translate(random(-1,1),random(-1,1))
   if (keyIsDown(DOWN_ARROW)){
     accel.y+=.002;
   }
-  if (isMobile) {
+  if (hasSensorPermission) {
       //Conditional script here
-      accel.y =rotationX*.01;
-      accel.x =rotationY*.01;
+      accel.y = accelerationX;
+      accel.x = accelerationY;
   }
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
-  }
 
 
 
@@ -223,7 +280,12 @@ Mass.prototype.wrapEdges = function() {
   }
 };
 
-
+function touchEnded() {
+  if (!hasSensorPermission){
+    begPermission();
+  }
+  return false;
+}
 
 function windowResized() {
   // resizeCanvas(windowWidth, windowHeight);
