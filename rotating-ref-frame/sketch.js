da7c54b74ea06ea63b1b757888ad36f4994bd21c
James Hedberg <jhedberg@ccny.cuny.edu>
2021-11-30 11:02:46 -0500

    Update sketch.js

--- rotating-ref-frame/sketch.js
+++ rotating-ref-frame/sketch.js
@@ -10,16 +10,26 @@ function setup() {
   frameRate(30)
   onoff = createButton("start");
   onoff.mouseClicked(turnonoff);
-  onoff.position(windowWidth*.9,30);
+  onoff.position(windowWidth*.85,30);
   onoff.class("sim-button");
   onoff.parent("sketch-holder")
   checkerBG = loadImage('checkerboard.jpg'); // Load the image
   imageMode(CENTER);
   reset = createButton("reset");
   reset.mouseClicked(resetSketch);
-  reset.position(windowWidth*.9,80);
+  reset.position(windowWidth*.85,80);
   reset.class("sim-button");
   reset.parent("sketch-holder")
+  if (width >= height){
+    toTranslate1 = createVector(-width/4,0)
+    toTranslate2 = createVector(+width/4,0)
+    diskRadius = width/6
+   }
+   else {
+     toTranslate1 = createVector(0,-height/6)
+     toTranslate2 = createVector(0,height/4)
+     diskRadius = height/8
+   }
 
   vslider = createSlider(0,3,(omegaGlobal*diskRadius)*sqrt(2),.01);
   vslider.style('width', '200px');
@@ -59,8 +69,8 @@ function makescene() {
     vel = createVector(velx,vely);
     accel = 0;
 
-    ball = new KineticMass(pos,vel,accel,20,'green');
-
+    ball = new KineticMass(pos,vel,accel,15,'RoyalBlue');
+    ball.outline = 'RoyalBlue';
   //This will be the position vector
   posVector = new Arrow(origin,pos);
   posVector.color = color('LightSkyBlue');
@@ -71,25 +81,41 @@ function makescene() {
   posVector2.width = 10;
   //And this will be the velocity vector
   velVector = new Arrow(pos,vel);
-  velVector.color = color('green');
+  velVector.color = color('MediumSeaGreen');
   velVector.width = 10;
   velVector.grab = false;
   velVector.draggable = false;
   velVector.showComponents = false;
+
+
   //velVector.showComponents = true;
   omegaVec = createVector(0,0,-omegaGlobal)
 
   ball2velrot = p5.Vector.sub(ball.velocity,omegaVec.cross(ball.position))
   ball2v0 = createVector(velx-speed,vely)
-  ball2 = new KineticMass(p5.Vector.add(origin2, createVector(0,diskRadius)),ball2v0,accel,20,'purple');
-
+  ball2 = new KineticMass(p5.Vector.add(origin2, createVector(0,diskRadius)),ball2v0,accel,15,'RoyalBlue');
+  ball2.outline = 'RoyalBlue';
+  velVectorRot = new Arrow(ball2.position,ball2velrot);
+  velVectorRot.color = color('MediumSeaGreen');
+  velVectorRot.width = 10;
+  velVectorRot.grab = false;
+  velVectorRot.draggable = false;
+  velVectorRot.showComponents = false;
   frame1 = new RotFrame(origin,omegaGlobal,ball);
+  frame1.label = "Inertial Frame"
   frame2 = new RotFrame(origin2,.0,ball2);
+  frame2.label = "Rotating Frame"
   push()
-translate(-width/4,0)
+translate(toTranslate1.x,toTranslate1.y)
 velVector.target = p5.Vector.add(p5.Vector.mult(createVector(velx,vely),50),ball.position);
   velVector.update();
   velVector.display();
+pop()
+push()
+translate(toTranslate2.x,toTranslate2.y)
+  velVectorRot.target = p5.Vector.add(p5.Vector.mult(createVector(ball2velrot.x,ball2velrot.y),50),ball2.position);
+    velVectorRot.update();
+    velVectorRot.display();
   pop()
 }
 
@@ -115,7 +141,7 @@ function draw() {
   background(250);
   translate(width/2,height/2)
   push()
-  translate(-width/4,0)
+  translate(toTranslate1.x,toTranslate1.y)
 
   if (running){
 frame1.update()
@@ -126,15 +152,19 @@ posVector.update();
 velVector.origin = ball.position;
 velVector.target = p5.Vector.add(p5.Vector.mult(ball.velocity,50),ball.position);
 velVector.update();
-}
-frame1.display();
-ball.display();
-posVector.display();
-velVector.display();
 
+
+
+}
+
+frame1.display();
+
+ball.display();
+ posVector.display();
+ velVector.display();
 pop()
 push()
-translate(+width/4,0)
+translate(toTranslate2.x,toTranslate2.y)
 
 if (running){
 frame2.update()
@@ -146,13 +176,17 @@ ball2.acceleration = p5.Vector.mult(totala, -1)
 ball2.update();
 posVector2.target = ball2.position;
 posVector2.update();
+velVectorRot.origin = ball2.position;
+velVectorRot.target = p5.Vector.add(p5.Vector.mult(createVector(ball2.velocity.x,ball2.velocity.y),50),ball2.position);
+  velVectorRot.update();
+
 }
 frame2.display();
 //noStroke()
 ball2.display();
 
 posVector2.display();
-
+  velVectorRot.display();
 pop()
 
   //omegaVec2 = createVector(0,0,0.01)
@@ -184,6 +218,7 @@ class RotFrame {
     this.trails2 = [];
     this.balltotrace = balltotrace
     this.c = 0;
+    this.label = 'frame'
 
   }
 
@@ -246,10 +281,13 @@ class RotFrame {
     strokeWeight(2);
     fill(200,200,200,240);
     translate(this.position.x,this.position.y)
-
     rotate(-this.rotation)
-
+    push()
+    strokeWeight(4)
+    stroke('red')
     circle(0, 0, this.radius*2);
+    pop()
+
     line(0-this.radius,0,0+this.radius,0)
     line(0,0-this.radius,0,0+this.radius-30)
     translate(0,this.radius)
@@ -264,8 +302,8 @@ class RotFrame {
     // for(var i=0;i<this.trails2.length;i++){
     //   ellipse(this.trails2[i].x,this.trails2[i].y,5);
     // }
-
-
+    fill(0)
+    text(this.label,-40,-diskRadius-60)
 
   }
 }
