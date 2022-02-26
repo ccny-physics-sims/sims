b72558dac1eebefc3962f8e4b5fd0da59a428172
James Hedberg <jhedberg@ccny.cuny.edu>
2022-02-26 08:09:18 -0500

    Update sketch.js

--- parallax/sketch.js
+++ parallax/sketch.js
@@ -16,7 +16,7 @@ function setup() {
 
 
   Slider = createSlider(0, PI, 0 ,.05);
-  Slider.position(50,height*.05);
+  Slider.position(50,height*.85);
   Slider.parent('sketch-holder')
   Slider.class("sim-slider gray");
   Slider.size(sliderWidth,0)
@@ -29,7 +29,7 @@ function setup() {
 
 AA = min(width/3,height/3)
   ASlider = createSlider(20, AA, 100 ,1);
-  ASlider.position(50,height*.15);
+  ASlider.position(50,height*.95);
   ASlider.parent('sketch-holder')
   ASlider.class("sim-slider gray");
   ASlider.size(sliderWidth,0)
@@ -47,9 +47,9 @@ function draw() {
   fill(255);
   A = ASlider.value()
   B = Slider.value()
-
+strokeWeight(1)
   drawingContext.setLineDash([]);
-  translate(width/4,height/2)
+  translate(width/5,height/2)
   sunPos = createVector(0,0)
   push()
   stroke('lightblue')
@@ -77,12 +77,12 @@ function draw() {
   translate(earthPos);
 
   text("Earth",-40,5);
-
+starLinex = .6*width;
   image(earthImage,0,0,15,15)
   pop();
   stroke(0)
   starPos = createVector(width/3,0)
-  line(sunPos.x,sunPos.y,width/2,0)
+  line(sunPos.x,sunPos.y,starLinex,0)
   stroke(150)
   line(sunPos.x,sunPos.y,0,A)
   line(sunPos.x,sunPos.y,0,-A)
@@ -91,26 +91,55 @@ function draw() {
   v3 = p5.Vector.sub(starPos,createVector(0,0));
   //v3.setMag(500)
   stroke(0)
+
   line(earthPos.x,earthPos.y,starPos.x,starPos.y)
-  star(starPos.x,0,10,5,5,'red')
-  star(width/2,-height/3,10,5,5,'lightgreen')
-  star(width/2,-height/6,10,5,5,'yellow')
-  star(width/2,0,10,5,5,'lightblue')
-  star(width/2,height/6,10,5,5,'orange')
-  star(width/2,height/3,10,5,5,'purple')
+  star2(starPos.x,0,10,5,5,'red')
+  star2(starLinex,-height*.1,10,5,5,'yellow')
+  star2(starLinex,-height*.2,10,5,5,'lightgreen')
+  star2(starLinex,0,10,5,5,'lightblue')
+  star2(starLinex,height*.1,10,5,5,'orange')
+  star2(starLinex,height*.2,10,5,5,'purple')
   drawingContext.setLineDash([2, 2]);
-  line(starPos.x,starPos.y,width/2,(width/2-starPos.x)*tan(atan2(-earthPos.y,starPos.x-earthPos.x)))
+
   //line(starPos.x,starPos.y,width/2,0)
-  star(width/2,(width/2-starPos.x)*tan(atan2(-earthPos.y,starPos.x-earthPos.x)),10,5,5,'pink')
+  line(starPos.x,starPos.y,starLinex,(starLinex-starPos.x)*tan(atan2(-earthPos.y,starPos.x-earthPos.x)))
+  star2(starLinex,(starLinex-starPos.x)*tan(atan2(-earthPos.y,starPos.x-earthPos.x)),10,5,5,'pink')
   fill(0)
   noStroke()
   text("d",starPos.x/2,-5);
 
   text("p",starPos.x+40,30*sin(atan2(-earthPos.y,starPos.x))+5);
+  drawingContext.setLineDash([]);
 
-
+  makeEyepiece(min(150,width/3))
 
   //drawArrow(earthPos, starPos, 'red');
+}
+
+function makeEyepiece(circleDia){
+  fill(0)
+  translate(width/4,-height/3)
+  text("Sky View",-25,-circleDia/1.8)
+  circle(0,0,circleDia)
+  star(0,0,8,4,5,'lightblue')
+  star(circleDia*.2,0,8,4,5,'orange')
+  star(circleDia*.4,0,8,4,5,'purple')
+  star(-circleDia*.2,0,8,4,5,'yellow')
+  star(-circleDia*.4,0,8,4,5,'lightgreen')
+  redStarx = 2*(circleDia/height)*(starLinex-starPos.x)*tan(atan2(-earthPos.y,starPos.x-earthPos.x))
+  if (abs(redStarx) < circleDia/2){
+  star(redStarx,0,8,4,5,'red')
+}
+stroke(255)
+  for (let i = 0;i<13;i++){
+    line(10*(i-6),8,10*(i-6),2)
+  }
+  strokeWeight(4)
+  stroke(150)
+  noFill()
+  circle(0,0,circleDia)
+
+
 }
 
 function drawArrow(base, vec, myColor) {
@@ -127,7 +156,7 @@ function drawArrow(base, vec, myColor) {
   pop();
 }
 
-function star(x, y, radius1, radius2, npoints,color) {
+function star2(x, y, radius1, radius2, npoints,color) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
   fill(color)
@@ -141,6 +170,23 @@ function star(x, y, radius1, radius2, npoints,color) {
     vertex(sx, sy);
   }
   endShape(CLOSE);
+}
+
+function star(x, y, radius1, radius2, npoints,color) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  fill(color)
+  circle(x,y,radius1)
+  // beginShape();
+  // for (let a = 0; a < TWO_PI; a += angle) {
+  //   let sx = x + cos(a) * radius2;
+  //   let sy = y + sin(a) * radius2;
+  //   vertex(sx, sy);
+  //   sx = x + cos(a + halfAngle) * radius1;
+  //   sy = y + sin(a + halfAngle) * radius1;
+  //   vertex(sx, sy);
+  // }
+  // endShape(CLOSE);
 }
 
 function sliderChanged() {
